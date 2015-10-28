f791c0fc7345b94e6e4737e4ea84e543c3f914aa
Zongsi Zhang <zongsizhang@Zongsis-MacBook-Pro.local>
2015-10-28 11:08:06 -0700

    initialte project

---
+++ FirmCompetition/src/com/cse556/project/Firm.java
@@ -0,0 +1,24 @@
+package com.cse556.project;
+
+public class Firm {
+	private double invest_Inno;			//innovation invest
+	private double invest_Qual;         //quality invest 
+	private double investment;			//total invest
+	
+	private double index_Ht;       //high-tech index
+	private double techAd;         //advance of tech index per year.
+	private double index_Ex;     //experience index
+	
+	private double prod_price;				//product price
+	private double prod_cost;				//product cost
+	
+	private double brandComp;			//brand competitive
+	private int sellVol;				//product sales volume
+	private double profit;				//final profit
+	
+	public void initiate(double invest0, double ht0, double ex0){
+		
+	}
+	
+	
+}
